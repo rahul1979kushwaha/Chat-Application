8e308a1efdac9b2d6dd84c537763e1ec652a5848
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2024-07-15 23:06:01 +0530

    signup page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -4,7 +4,15 @@ function Signup() {
     return (
         <div className='min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
-                <h1 className='text-3xl font-bold text-center text-gray-300'>Signup</h1>
+                <h1 className='text-3xl font-bold text-center text-black'>Signup</h1>
+                <form action=''>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Full Name</span>
+                        </label>
+                        <input className='w-full input input-bordered h-10' type='text' placeholder='Enter name'></input>
+                    </div>
+                </form>
             </div>
         </div>
     )
