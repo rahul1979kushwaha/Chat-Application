a1d638b671426ac370df091be937e1cbdccaae2d
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2024-07-15 08:11:43 +0530

    first commit

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <div className='min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
+                <h1 className='text-3xl font-bold text-center text-gray-300'>Signup</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
