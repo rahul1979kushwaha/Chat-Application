9dffb43ff8afc6c864f84588daae41de1fd75894
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2024-08-06 10:35:29 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import HomePage from './components/HomePage'
-import Signup from './components/Signup'
-import Login from './components/Login'
+import Signup from './components/Signup';
+import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import { useEffect, useState } from 'react';
+import {useSelector,useDispatch} from "react-redux";
+import io from "socket.io-client";
+import { setSocket } from './redux/socketSlice';
+import { setOnlineUsers } from './redux/userSlice';
+import { BASE_URL } from './main';
 
 const router = createBrowserRouter([
   {
@@ -10,22 +16,49 @@ const router = createBrowserRouter([
     element:<HomePage/>
   },
   {
-    path:"/register",
+    path:"/signup",
     element:<Signup/>
   },
   {
     path:"/login",
     element:<Login/>
-  }
-])  
-function App() {
+  },
+
+])
+
+function App() { 
+  const {authUser} = useSelector(store=>store.user);
+  const {socket} = useSelector(store=>store.socket);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(authUser){
+      const socketio = io(`${BASE_URL}`, {
+          query:{
+            userId:authUser._id
+          }
+      });
+      dispatch(setSocket(socketio));
+
+      socketio?.on('getOnlineUsers', (onlineUsers)=>{
+        dispatch(setOnlineUsers(onlineUsers))
+      });
+      return () => socketio.close();
+    }else{
+      if(socket){
+        socket.close();
+        dispatch(setSocket(null));
+      }
+    }
+
+  },[authUser]);
+
   return (
-    <>
-    <div className='p-4 h-screen flex items-center justify-center'>
+    <div className="p-4 h-screen flex items-center justify-center">
       <RouterProvider router={router}/>
     </div>
-    </>
-  )
+
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,53 +1,76 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-function Login() {
-    const [user, setUser] = useState({
-        username: "",
-        password: "",
-    })
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
-        console.log(user);
-        setUser({
-            username: "",
-            password: "",
-        })
+import { Link, useNavigate } from 'react-router-dom'
+import toast from "react-hot-toast"
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from '../redux/userSlice';
+import { BASE_URL } from '../main';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${BASE_URL}/api/v1/user/login`, user, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      });
+      navigate("/");
+      console.log(res);
+      dispatch(setAuthUser(res.data));
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
     }
-    return (
-        <div className='min-w-96 mx-auto'>
-            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
-                <h1 className='text-3xl font-bold text-center text-black'>Login</h1>
-                <form onSubmit={onSubmitHandler} action=''>
-                    <div>
-                        <label className='label p-2'>
-                            <span className='text-base label-text text-black'>Username</span>
-                        </label>
-                        <input
-                            value={user.username}
-                            onChange={(e) => setUser({ ...user, username: e.target.value })}
-                            className='w-full input input-bordered h-10'
-                            type="text"
-                            placeholder='Username' />
-                    </div>
-                    <div>
-                        <label className='label p-2'>
-                            <span className='text-base label-text text-black'>Password</span>
-                        </label>
-                        <input
-                            value={user.password}
-                            onChange={(e) => setUser({ ...user, password: e.target.value })}
-                            className='w-full input input-bordered h-10'
-                            type="password"
-                            placeholder='Password' />
-                    </div>
-                    <p className='text-black text-center my-2'>New User? <Link to="/register">Register</Link></p>
-                    <div>
-                        <button className='btn btn-block btn-md mt-2 border border-slate-700' type='submit'>Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+    setUser({
+      username: "",
+      password: ""
+    })
+  }
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
+        <h1 className='text-3xl font-bold text-center'>Login</h1>
+        <form onSubmit={onSubmitHandler} action="">
+
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              className='w-full input input-bordered h-10'
+              type="text"
+              placeholder='Username' />
+          </div>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className='w-full input input-bordered h-10'
+              type="password"
+              placeholder='Password' />
+          </div>
+          <p className='text-center my-2'>Don't have an account? <Link to="/signup"> signup </Link></p>
+          <div>
+            <button type="submit" className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
